c0d08b3ec849a057a0fe5f0f3081a9217870d305
Jordan Budisantoso <jordanbudi@gmail.com>
2015-06-17 13:58:04 -0500

    Add HTML to index

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,17 @@
+#shotgun -p 3000 -o 0.0.0.0
 require 'bundler'
+require 'Sinatra'
 Bundler.require
+require_relative 'models/Madlib.rb'
 
 class ApplicationController < Sinatra::Base
 
   get '/' do
     erb :index
+  end
+
+  post '/results' do
+    erb :results
   end
 
 end
---
+++ models/Madlib.rb
@@ -0,0 +1,13 @@
+#authentic, cheap, expensive, ridiculous
+
+male_celeb = ["Bradley Cooper", "Adam Levine", "Thor", "Will Ferrel", "Kanye West"]
+female_celeb = ["Shakira", "Jennifer Lawrence", "Natalie Portman", "Bjork", "Kerry Washington", "Catwoman"]
+restaurant = ["McDonalds", "Momma's home cooking", "Olive Garden", "Dinner Under the Eiffel Tower", "Elon Musk Serving caviar in Space"]
+activity = ["Gondola in Venice", "International Banana Museum", "Helicopter Ride"]
+event = []
+
+=begin
+  
+1. 
+  
+=end
